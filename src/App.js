96acabfc6c256f0ce574d27e915e8dfb549e5970
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-07-08 21:49:01 +0530

    add category component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
 
 const App = () => {
   return (
-    <div className="App">
-      Hello World
+    <div className="categories-container">
+      <div className="category-container">
+        {/* <img/> */}
+        <div className="category-body-container">
+          <h1>Hats</h1>
+          <p>Shop Now!</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img/> */}
+        <div className="category-body-container">
+          <h1>Jackets</h1>
+          <p>Shop Now!</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img/> */}
+        <div className="category-body-container">
+          <h1>Shoes</h1>
+          <p>Shop Now!</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img/> */}
+        <div className="category-body-container">
+          <h1>Womens</h1>
+          <p>Shop Now!</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img/> */}
+        <div className="category-body-container">
+          <h1>Mens</h1>
+          <p>Shop Now!</p>
+        </div>
+      </div>
     </div>
   );
 }
