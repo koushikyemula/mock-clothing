b3530b11113526baa8cd90891c56e0cf72029da3
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-07-08 22:18:37 +0530

    category mapp

--- src/App.js
+++ src/App.js
@@ -1,42 +1,38 @@
 
 const App = () => {
+  const categories = [
+    {
+      id:1,
+      title:"Hats",
+    },
+    {
+      id:2,
+      title:"Jackets",
+    },
+    {
+      id:3,
+      title:"Shoes",
+    },
+    {
+      id:4,
+      title:"Womens",
+    },
+    {
+      id:5,
+      title:"Mens",
+    },
+  ]
   return (
     <div className="categories-container">
+      {categories.map(({title}) => (
       <div className="category-container">
         {/* <img/> */}
         <div className="category-body-container">
-          <h1>Hats</h1>
+          <h1>{title}</h1>
           <p>Shop Now!</p>
         </div>
       </div>
-      <div className="category-container">
-        {/* <img/> */}
-        <div className="category-body-container">
-          <h1>Jackets</h1>
-          <p>Shop Now!</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img/> */}
-        <div className="category-body-container">
-          <h1>Shoes</h1>
-          <p>Shop Now!</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img/> */}
-        <div className="category-body-container">
-          <h1>Womens</h1>
-          <p>Shop Now!</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img/> */}
-        <div className="category-body-container">
-          <h1>Mens</h1>
-          <p>Shop Now!</p>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
