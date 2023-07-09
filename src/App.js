2542592662557aa57fd16289e0b7328b89d2e648
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-07-09 23:13:38 +0530

    add navigation route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import Home from "./routes/home/home.component";
-import { Routes, Route, Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
 
+const Shop = () => {
+  return (
+    <div>
+      <h1>shoping</h1>
+    </div>
+  );
+};
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
