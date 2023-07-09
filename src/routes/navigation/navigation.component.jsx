a5d51d8dfa47e972927c71b96c382cf6a5cbc9d1
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-07-09 23:33:46 +0530

    add saas style

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,14 @@
 import { Outlet, Link } from "react-router-dom";
 import { Fragment } from "react";
+import { ReactComponent as MockLogo} from "../../assets/007 crown.svg"
+import "./navigation.styles.scss"
 
 const Navigation = () => {
     return (
         <Fragment>
             <div className="navigation">
                 <Link className="logo-container" to="/">
-                    <div>Logo</div>
+                    <MockLogo/>
                 </Link>
                 <div className="nav-links-container">
                     <Link className="nav-link" to="/shop">
