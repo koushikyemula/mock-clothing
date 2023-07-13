353ae85972bdd984e62dda79b59887f2f13873a2
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-07-13 20:30:54 +0530

    add signin route

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/signin" element={<SignIn />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
