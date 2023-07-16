f4feaa92c8691891978f3efd8807789da7a1485c
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-07-16 20:34:33 +0530

    redirect error

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,16 +1,27 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils"
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth"
+import { signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect, auth } from "../../utils/firebase/firebase.utils"
 
 const SignIn = () => {
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup()
         const userDocRef = await createUserDocumentFromAuth(user)
-        
     }
+
+    useEffect(async () => {
+        const response = await getRedirectResult(auth)
+        if (response) {
+            const userDocRef = await createUserDocumentFromAuth(response.user)
+        }
+    }, []);
     return (
         <div>
             <h1>sign in</h1>
             <button onClick={logGoogleUser}>
                 sign in with google popup
+            </button>
+            <button onClick={signInWithGoogleRedirect}>
+                sign in with google redirect
             </button>
         </div>
     )
