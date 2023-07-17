48ddd06bd891a787aae8c1e4a98a99cca4c72b21
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-07-17 18:36:43 +0530

    add authentication styles

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,13 +1,11 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils"
-import SignUpForm from "../../components/sign-up-form/sign-up-form.component"
-import Button from "../../components/button/button.component"
-import SignInForm from "../../components/sign-in-form/sign-in-form-component"
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import SignInForm from "../../components/sign-in-form/sign-in-form-component";
+import "./authentication.styles.scss"
 
 const Authentication = () => {
     
     return (
-        <div>
-            <h1>Sign In</h1>
+        <div className="authentication-container">
             <SignInForm />
             <SignUpForm />
         </div>
