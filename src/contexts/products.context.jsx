dd8e1a7d24d6f62c0d3ae77ce6998bbf3e82f42f
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-07-21 21:20:05 +0530

    product card and styling , shop styling , products provider and minor changes

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -2,12 +2,14 @@ import { createContext } from "react";
 import { useState } from "react";
 import PRODUCTS from "../shop-data.json";
 
-export const productContext = createContext({})
+export const ProductsContext = createContext({
+    products: [],
+})
 
 export const ProductsProvider = ({children}) => {
     const [products, setProducts] = useState(PRODUCTS);
     const value = {products};
     return (
-        <productContext.Provider value={value}>{children}</productContext.Provider>
+        <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
     )
 }
