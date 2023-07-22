72781e2fc46549e8428efae6fb688c9fb87fd4c4
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-07-22 15:00:46 +0530

    cart toggle

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,12 +3,14 @@ import { Fragment,useContext } from "react";
 import { ReactComponent as MockLogo} from "../../assets/koushik_logo.svg"
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
 import "./navigation.styles.scss"
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 const Navigation = () => {
     const {currentUser} = useContext(UserContext)
+    const {isCartOpen} = useContext(CartContext)
     
     return (
         <Fragment>
@@ -28,7 +30,7 @@ const Navigation = () => {
                     }
                     <CartIcon/>
                 </div>
-                <CartDropdown/>
+                    {isCartOpen && <CartDropdown/>}
             </div>
             <Outlet/>
         </Fragment>
