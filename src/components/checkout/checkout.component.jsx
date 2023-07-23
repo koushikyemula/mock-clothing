b9e67188537391028aa468d572c17dcb53c6d4ab
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-07-23 21:20:28 +0530

    checkout page styling

--- src/components/checkout/checkout.component.jsx
+++ src/components/checkout/checkout.component.jsx
@@ -5,9 +5,24 @@ import { CartContext } from "../../contexts/cart.context";
 const Checkout = () => {
     const {cartItems, addItemToCart, removeItemFromCart} = useContext(CartContext)
     return (
-        <div>
-            <h1>checkout</h1>
-        <div>
+        <div className="checkout-container">
+            <div className="checkout-header">
+                <div className="header-block">
+                    <span>Product</span>
+                </div>
+                <div className="header-block">
+                    <span>Description</span>
+                </div>
+                <div className="header-block">
+                    <span>Quantity</span>
+                </div>
+                <div className="header-block">
+                    <span>Price</span>
+                </div>
+                <div className="header-block">
+                    <span>Remove</span>
+                </div>
+            </div>
             {cartItems.map((cartItem) => {
                 const {id, name, quantity} = cartItem
                 return (
@@ -21,7 +36,7 @@ const Checkout = () => {
                     </div>
                 )
             })}
-        </div>
+            <span className="Total">Total:0</span>
         </div>
     )
 }
