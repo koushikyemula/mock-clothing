d061eff225910503ca1bb513410eab80fce6adc4
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-08-01 23:54:52 +0530

    reselect test p1

--- src/store/category/category-selector.js
+++ src/store/category/category-selector.js
@@ -1,1 +1,10 @@
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.categories
+);
+
 export const selectCategoriesMap = (state) => state.categories.categoriesMap;
