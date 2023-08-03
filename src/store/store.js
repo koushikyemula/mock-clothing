28f5098b224b7ebd9c137986978a497102075bb7
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-08-03 19:51:33 +0530

    logger middleware custom

--- src/store/store.js
+++ src/store/store.js
@@ -3,10 +3,23 @@ import {
   legacy_createStore as createStore,
   applyMiddleware,
 } from "redux";
-import { logger } from "redux-logger";
 import { rootReducer } from "./root-reducer";
 
-const middleWares = [logger];
+const loggerMiddleWare = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+
+  console.log("type:", action.type);
+  console.log("payload", action.payload);
+  console.log("currentState:", store.getState());
+
+  next(action);
+
+  console.log("nextState:", store.getState());
+};
+
+const middleWares = [loggerMiddleWare];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
