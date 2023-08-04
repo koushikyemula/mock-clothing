1d1f034d80f9fa6f3c7db1c15789cd5c6255152b
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-08-04 23:01:51 +0530

    redux thunk fetchcategories async

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,7 +1,6 @@
 import { Routes,Route } from "react-router-dom";
 import { useEffect } from "react";
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
-import { setCategoriesMap } from "../../store/category/category-action";
+import { fetchCategoriesAsync } from "../../store/category/category-action";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
 import "./shop.styles.scss";
@@ -11,12 +10,8 @@ const Shop = () => {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        const getCategoriesMap = async () => {
-            const categoryMap = await getCategoriesAndDocuments();
-            dispatch(setCategoriesMap(categoryMap));
-        }
-        getCategoriesMap()
-    }, [dispatch])
+        dispatch(fetchCategoriesAsync());
+    }, [dispatch]);
 
     return (
         <Routes>
--- src/store/category/category-action.js
+++ src/store/category/category-action.js
@@ -1,5 +1,23 @@
 import { createAction } from "../../utils/reducer-utils";
 import { CATEGORY_ACTION_TYPES } from "./category-types";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 
-export const setCategoriesMap = (categoriesMap) =>
-  createAction(CATEGORY_ACTION_TYPES.SET_CATEGORIES_MAP, categoriesMap);
+export const fetchCategoriesStart = () =>
+  createAction(CATEGORY_ACTION_TYPES.FETCH_CATEGORIES_START);
+
+export const fetchCategoriesSuccess = (categoriesMap) =>
+  createAction(CATEGORY_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categoriesMap);
+
+export const fetchCategoriesFailed = (error) =>
+  createAction(CATEGORY_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
+
+export const fetchCategoriesAsync = () => async (dispatch) => {
+  dispatch(fetchCategoriesStart());
+
+  try {
+    const categoryMap = await getCategoriesAndDocuments("collections");
+    dispatch(fetchCategoriesSuccess(categoryMap));
+  } catch (error) {
+    dispatch(fetchCategoriesFailed(error));
+  }
+};
